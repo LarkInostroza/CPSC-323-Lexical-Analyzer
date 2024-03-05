554e06807104940c7c9a088c834d7e577d905cfc
LarkInostroza <147445450+LarkInostroza@users.noreply.github.com>
2024-03-04 19:04:58 -0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cctype>
 
+
 //Token struct (Will store each token's type and value)
 struct Token {
     std::string type;//:3
@@ -11,6 +12,9 @@ struct Token {
 
 //Declaring Lexer Function
 Token lexer(std::ifstream& inputFile);
+bool isKeyWord(std::string current);
+bool isOperator(char current);
+bool isSeparator(char current);
 
 int main() {
     std::ifstream inputFile("input.txt");
@@ -27,6 +31,45 @@ int main() {
 
     return 0;
 }
+bool isOperator(char current) {
+  return (current == '+') || (current == '-') || (current == '*') || (current == '/') || (current == '%') ||
+     (current == '?') || (current == ',') || (current == '&') || (current == '|') ||
+     (current == '^') || (current == '~') || (current == '<') || (current == '>');
+}
+bool isSeparator(char current) {
+    const char separators[] = { ';', ',', ':', '.', '-', '>', '(', ')', '[', ']', '{', '}', '#', '\\'};
+    for (char separator : separators) {
+        if (current == separator) {
+            return true;
+        }
+    }
+    return false;
+}
+bool isKeyword(std::string current) {
+    static const std::string keywords[] = {
+        "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor",
+        "bool", "break", "case", "catch", "char", "char8_t", "char16_t", "char32_t",
+        "class", "compl", "concept", "const", "consteval", "constexpr", "const_cast",
+        "continue", "co_await", "co_return", "co_yield", "decltype", "default",
+        "delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export",
+        "extern", "false", "float", "for", "friend", "goto", "if", "inline", "int",
+        "long", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+        "operator", "or", "or_eq", "private", "protected", "public", "register",
+        "reinterpret_cast", "requires", "return", "short", "signed", "sizeof", "static",
+        "static_assert", "static_cast", "struct", "switch", "synchronized", "template",
+        "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename",
+        "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while",
+        "xor", "xor_eq"
+    };
+    for (const std::string& keyword : keywords) {
+        if (current == keyword) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 
 //The world's worst switch statement called a lexer
 Token lexer(std::ifstream& inputFile) {
@@ -51,11 +94,11 @@ Token lexer(std::ifstream& inputFile) {
                 else if (std::isspace(currentChar)) {
                     // Skip whitespace
                 } 
-                else if (currentChar == '(' || currentChar == ')' || currentChar == ';') {
+                else if (isSeparator(currentChar)) {
                     lexeme += currentChar;
                     return { "Separator", lexeme };
                 } 
-                else if (currentChar == '=') {
+                else if (isOperator(currentChar)) {
                     lexeme += currentChar;
                     return { "Operator", lexeme };
                 } 
@@ -70,7 +113,7 @@ Token lexer(std::ifstream& inputFile) {
                 } 
                 else {
                     inputFile.unget(); // Return currentChar to stream
-                    if (lexeme == "while" || lexeme == "if" || lexeme == "else") {
+                    if (isKeyword(lexeme)) {
                         return { "Keyword", lexeme };
                     }
                     return { "Identifier", lexeme };
@@ -85,28 +128,28 @@ Token lexer(std::ifstream& inputFile) {
                     lexeme += currentChar;
                 } 
                 else {
-                    inputFile.unget(); // Return currentChar to stream
+                    inputFile.unget(); 
                     return { "Integer", lexeme };
                 }
                 break;
-            case State::IN_REAL: //Handles real numbers (floating point and ect)
+            case State::IN_REAL:
                 if (std::isdigit(currentChar)) {
                     lexeme += currentChar;
                 } 
                 else {
-                    inputFile.unget(); // Return currentChar to stream
+                    inputFile.unget(); //Real as in real number
                     return { "Real", lexeme };
                 }
                 break;
             case State::IN_OPERATOR:
-                // Handle other operators
+                //Redundancy
                 lexeme += currentChar;
                 return { "Operator", lexeme };
             case State::DONE:
-                break;//:3
+                break;
         }
     }
-
     // End of file reached
     return { "EOF", "" };
 }
+
